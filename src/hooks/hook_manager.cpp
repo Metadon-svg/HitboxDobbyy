b045c8311cd9b9ecb58a8df5343d9cdf3f3fdef9
Metadon-svg <krisevicu@gmail.com>
2026-07-19 02:51:19 +0300

    Update hook_manager.cpp

--- src/hooks/hook_manager.cpp
+++ src/hooks/hook_manager.cpp
@@ -10,12 +10,12 @@ bool HookManager::Init() {
 
 bool HookManager::Hook(uintptr_t target, void* replacement, void** original) {
     if (!s_initialized && !Init()) return false;
-    return DobbyHook((void*)target, replacement, original) == 0;
+    return DobbyHook((DobbyAddr)target, (DobbyAddr)replacement, (DobbyAddr*)original) == 0;
 }
 
 bool HookManager::Unhook(uintptr_t target) {
     if (!s_initialized) return false;
-    return DobbyUnhook((void*)target) == 0;
+    return DobbyUnhook((DobbyAddr)target) == 0;
 }
 
 bool HookManager::IsReady() {
